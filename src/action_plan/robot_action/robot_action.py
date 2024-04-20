2944259c5224bb9b9b40fce9767086aa2c75d182
crothg <164100907+crothg@users.noreply.github.com>
2024-04-20 14:28:07 -0400

    Update robot_action.py

--- src/action_plan/robot_action/robot_action.py
+++ src/action_plan/robot_action/robot_action.py
@@ -9,18 +9,20 @@ import numpy as np
 from std_msgs.msg import String
 
 
-class MinimalPublisher(Node):
+class WaypointPublisher(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
+        super().__init__('waypoint_publisher')
         self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
+        waypoint.data = [3.0,3.0,np.pi/2]
+        ""
         msg.data = 'Hello World: %d' % self.i
+        ""
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
@@ -29,14 +31,14 @@ class MinimalPublisher(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    waypoint_publisher = WaypointPublisher()
 
-    rclpy.spin(minimal_publisher)
+    rclpy.spin(waypoint_publisher)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    waypoint_publisher.destroy_node()
     rclpy.shutdown()
 
 
