f9c7b163bc3336410deb8e9c01a45bd1f4fc1ea4
crothg <crothg@rpi.edu>
2024-04-20 15:36:26 -0400

    change setup.py

--- src/action_plan/setup.py
+++ src/action_plan/setup.py
@@ -1,11 +1,29 @@
-maintainer='YourName',
-maintainer_email='you@email.com',
-description='Examples of minimal publisher/subscriber using rclpy',
-license='Apache License 2.0',
+from setuptools import setup
+import os
+from glob import glob
 
-entry_points={
+package_name = 'tracking_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yahboom',
+    maintainer_email='brandonfang412@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
         'console_scripts': [
-                'talker = robot_action.robot_action:main',
-                'listener = robot_action.subscriber_member_function:main',
+            'action_node = robot_action.robot_action:main',
         ],
-},
+    },
+)
