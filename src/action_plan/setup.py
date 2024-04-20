086e002fbea1d878536002ef2097b71e31024f7e
crothg <164100907+crothg@users.noreply.github.com>
2024-04-20 03:38:27 -0400

    Update setup.py

--- src/action_plan/setup.py
+++ src/action_plan/setup.py
@@ -1,1 +1,10 @@
+maintainer='YourName',
+maintainer_email='you@email.com',
+description='Examples of minimal publisher/subscriber using rclpy',
+license='Apache License 2.0',
 
+entry_points={
+        'console_scripts': [
+                'talker = py_pubsub.publisher_member_function:main',
+        ],
+},
