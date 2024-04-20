01e7b3085f623e20ad0fa6f467c8e330ea773433
crothg <164100907+crothg@users.noreply.github.com>
2024-04-20 14:55:54 -0400

    Update robot_action.py

--- src/action_plan/robot_action/robot_action.py
+++ src/action_plan/robot_action/robot_action.py
@@ -1,27 +1,25 @@
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
-from geometry_msgs.msg import PoseStamped, Twist, Quaternion
-from nav2_msgs.action import NavigateToPose
-from nav_msgs.msg import OccupancyGrid
 import numpy as np
 
-from std_msgs.msg import String
+from std_msgs.msg import Int32MultiArray
 
 
 class WaypointPublisher(Node):
 
     def __init__(self):
         super().__init__('waypoint_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(Int32MultiArray, 'topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
+        waypoint = Int32MultiArray()
         waypoint.data = [3.0,3.0,np.pi/2]
         self.publisher_.publish(waypoint)
-        self.get_logger().info('Publishing: "%s"' % waypoint.data)
+        pub.publish(waypoint)
         self.i += 1
         """
         msg = String()
