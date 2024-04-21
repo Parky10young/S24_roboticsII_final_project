41b42a6376eedfa1f1132f538ad50827d54d1408
crothg <crothg@rpi.edu>
2024-04-21 00:41:43 -0400

    Update robot_action.py

--- src/tracking_control/tracking_control/robot_action.py
+++ src/tracking_control/tracking_control/robot_action.py
@@ -4,6 +4,7 @@ from rclpy.node import Node
 import numpy as np
 
 from std_msgs.msg import String, Float32MultiArray
+from geometry_msgs.msg import PoseStamped
 
 
 class WaypointPublisher(Node):
@@ -13,32 +14,46 @@ class WaypointPublisher(Node):
         self.publisher_ = self.create_publisher(Float32MultiArray, 'waypoint_topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.i = 1
 
-        """self.current_pos_subscriber = self.create_subscription(String, '/map', self.map_callback, 10)"""
+        self.current_pos_subscriber = self.create_subscription(String, '/nav2_status', self.current_position_callback, 10)
+        self.object_pose_subscriber = self.create_subscription(PoseStamped, '/detected_color_object_pose', self.object_pose_callback, 10)
 
     def timer_callback(self):
-        waypoint = Float32MultiArray()
-        waypoint.data = [3.0,3.0,np.pi/2]
-        self.publisher_.publish(waypoint)
-        self.get_logger().info('Publishing: "%s"' % waypoint.data)
-        self.i += 1
+        #loop between waypoint 1 and start position
+        #go to waypoint 1
+        if self.i==1:
+            waypoint = Float32MultiArray()
+            waypoint.data = [3.0,3.0,np.pi/2]
+            self.publisher_.publish(waypoint)
+            self.get_logger().info('Publishing: "%s"' % waypoint.data)
+            self.i == 1
+            if  curr_pos== "Arrived":
+                self.i = 0
+                return
+            return
+        #go to start
+        else if self.i==0:
+            waypoint = Float32MultiArray()
+            waypoint.data = [0.0,0.0,0.0]
+            self.publisher_.publish(waypoint)
+            self.get_logger().info('Publishing: "%s"' % waypoint.data)
+            self.i = 0
+            if  curr_pos== "Arrived":
+                self.i = 1
+                return
+            return
 
-        """
-        if self.current_pos_subscriber == "Yes
         
-        """"
+    def current_position_callback(self, msg):
+        #check if robot has arrived to waypoint
+        curr_pos = msg.data
 
-
-
-        """
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
-        """
-
+    def object_pose_callback(self, pose):
+        #print object coordinates 
+        """add something here to get x,y,theta from pose data"""
+        print("Object Position")
+        print("X:",x,"y:",y,"yaw:",yaw)
 
 def main(args=None):
     rclpy.init(args=args)
